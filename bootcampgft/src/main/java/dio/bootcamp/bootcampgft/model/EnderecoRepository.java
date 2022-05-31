555dfd33013afa698ecb98a8d5e5d5e121977da6
Nícalo Ribeiro <nicalo63@gmail.com>
2022-05-31 02:10:46 -0300

    Criacao da classe, repository de endereco e servico para cep

---
+++ bootcampgft/src/main/java/dio/bootcamp/bootcampgft/model/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package dio.bootcamp.bootcampgft.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String>{
+    
+}
---
+++ bootcampgft/src/main/java/dio/bootcamp/bootcampgft/service/ViaCepService.java
@@ -0,0 +1,15 @@
+package dio.bootcamp.bootcampgft.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import dio.bootcamp.bootcampgft.model.Endereco;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
