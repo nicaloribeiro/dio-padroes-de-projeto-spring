3630afff22767eec5f894aedf85760a4ed922ac7
Nícalo Ribeiro <nicalo63@gmail.com>
2022-05-31 10:51:58 -0300

    Implementando services

--- bootcampgft/src/main/java/dio/bootcamp/bootcampgft/service/impl/ClienteServiceImpl.java
+++ bootcampgft/src/main/java/dio/bootcamp/bootcampgft/service/impl/ClienteServiceImpl.java
@@ -1,29 +1,67 @@
 package dio.bootcamp.bootcampgft.service.impl;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dio.bootcamp.bootcampgft.model.Cliente;
+import dio.bootcamp.bootcampgft.model.ClienteRepository;
+import dio.bootcamp.bootcampgft.model.Endereco;
+import dio.bootcamp.bootcampgft.model.EnderecoRepository;
 import dio.bootcamp.bootcampgft.service.ClienteService;
+import dio.bootcamp.bootcampgft.service.ViaCepService;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private ViaCepService viaCepService;
+
     @Override
     public Iterable<Cliente> buscarTodos() {
-        return null;
+        return clienteRepository.findAll();
     }
 
     @Override
     public Cliente buscarPorId(Long id) {
-        return null;
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        return cliente.get();
     }
 
     @Override
-    public void cadastrarCliente(Cliente cliente) {}
+    public void cadastrarCliente(Cliente cliente) {
+        verificaESalvaCliente(cliente);
+    }
 
     @Override
-    public void atualizarCliente(Long id, Cliente cliente){}
+    public void atualizarCliente(Long id, Cliente cliente) {
+        Optional<Cliente> clienteDb = clienteRepository.findById(id);
+        if (clienteDb.isPresent()) {
+            verificaESalvaCliente(cliente);
+        }
+    }
 
     @Override
-    public void deletarCliente(Long id) {}
+    public void deletarCliente(Long id) {
+        clienteRepository.deleteById(id);
+    }
+
+    
+    private void verificaESalvaCliente(Cliente cliente) {
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+            Endereco novoEndereco = viaCepService.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
+    }
 }
